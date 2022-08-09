18e0b8e29b4f9c0c7fab4f38df4779464c389af5
codewithrohan07 <rohankadu1999@gmail.com>
2022-08-09 14:43:13 +0530

    cart-item added

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,12 +1,22 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOPen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartitems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartitems(addCartItem(cardItems, productToAdd));
+  };
+
   const value = { isCartOpen, setIsCartOpen };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
