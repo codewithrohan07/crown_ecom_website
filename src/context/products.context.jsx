6913274906ddb809b51e82bfd09578855cfc610f
codewithrohan07 <rohankadu1999@gmail.com>
2022-08-20 15:08:22 +0530

    added shop-data to firebase database

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,15 +1,29 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-import PRODUCTS from "../shop-data.json";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+
+// import { SHOP_DATA } from "./../shop-data";
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
-  const value = { products };
+  const [products, setProducts] = useState([]);
 
+  // useEffect(() => {
+  //   addCollectionAndDocuments("categories", SHOP_DATA);
+  // }, []);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+
+  const value = { products };
   return (
     <ProductsContext.Provider value={value}>
       {children}
